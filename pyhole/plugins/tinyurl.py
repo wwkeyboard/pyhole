b0dd24bedec1b3f8d4c31e7c99a08ffa4d3332d6
Aaron Lee <aaron.lee@rackspace.com>
2012-03-23 04:31:18 -0500

    Adding TinyURL plugin to quiet Trey's whining.

---
+++ pyhole/plugins/tinyurl.py
@@ -0,0 +1,36 @@
+#   Copyright 2010-2011 Josh Kearney
+#   Copyright 2012 Aaron Lee
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""Pyhole VersionOne Plugin"""
+
+import urllib
+
+from pyhole import plugin
+
+class TinyURL(plugin.Plugin):
+    """Provides a short url when a long one is posted"""
+
+    def __init__(self, irc):
+        self.irc = irc
+        self.name = self.__class__.__name__
+
+    @plugin.hook_add_keyword("http://")
+    def http(self, params=None, **kwargs):
+        print params
+        if len(params) > 20:
+            tiny_api = ("http://tinyurl.com/api-create.php?url=" + 
+                           urllib.quote_plus(params))
+            print "requesting tinyurl from " + tiny_api
+            self.irc.reply(urllib.urlopen(tiny_api).read())
